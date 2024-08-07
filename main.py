a805e33a30e23f08a5a16604d360d36fca5e7855
SHubinaOlga <olga.shubina85@yandex.ru>
2024-08-07 19:24:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+home_work = 12
+number_of_hours = 1.5
+name = 'Python'
+time_spent = ((number_of_hours/home_work))
+
+print("Количество выполненых ДЗ - ", home_work, "; ", "Количество затраченных часов - " , number_of_hours, "; ", "Название курса - ", name, "; ", "Время на одно задание - ", time_spent)
